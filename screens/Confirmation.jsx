f92d02f8422e5299178f85716095aec6db101bd6
Laurent Kadeko <115667702+LaurentKadeko22@users.noreply.github.com>
2024-04-29 15:59:31 +0200

    working with social media icons

--- screens/Confirmation.jsx
+++ screens/Confirmation.jsx
@@ -3,12 +3,20 @@ import React from 'react'
 
 const FrontImage2 = require("../assets/frontside.png");
 
+
 const Confirmation = ({navigation}) => {
   return (
     <View style={styles.content}>
       <Text style={styles.headline}></Text>
       <Image source={FrontImage2} style={styles.image} />
-      {/* <Button title="Go to Home" onPress={() => navigation.navigate('Home')} /> */}
+      <Text style={styles.phoneNumber}>Phone Number</Text>
+      <View style={styles.loginContainer}>
+        <Text style={styles.Textinfo}>Or sign Up with</Text>
+        <View style={styles.loginOptions}>
+          <Text style={{flex: 1, textAlign: "center"}}>Facebook</Text>
+        </View>
+      </View>
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
     </View>
   )
 }
@@ -20,13 +28,44 @@ const styles = StyleSheet.create({
     backgroundColor: "#383838",
     height: "100%"
   },
-  headline:{
-    color: "green",
-    textAlign: "center"
-  },
+
   image: {
     position: "relative",
     left: "10%",
-    top: "5%"
-  }
+    top: "5%", 
+  },
+  phoneNumber: {
+    position: "relative",
+    left: 20,
+    top: "30%",
+    color: "white",
+    fontWeight: 500,
+    fontSize: 20
+  },
+  loginContainer: {
+    backgroundColor: "#22272B",
+    position: "absolute",
+    top: 520,
+    width: "100%",
+    height: "100%",
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40
+  },
+  Textinfo:{
+    textAlign: "center",
+    color: "#727272",
+    padding: 20,
+    fontSize: 20,
+    fontWeight: "500"
+  },
+  loginOptions: {
+    width: 80,
+    height: 80,
+    backgroundColor: "grey",
+    left: 30,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+  },
 })
